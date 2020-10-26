cd1e90a69d5384e492b01d94c82011e17d677119
Kasa <xjs513@163.com>
2020-10-26 09:43:58 +0800

    update

--- flink-learn-project/src/main/scala/com/atguigu/wc/WordCount.scala
+++ flink-learn-project/src/main/scala/com/atguigu/wc/WordCount.scala
@@ -1,21 +1,9 @@
 package com.atguigu.wc
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:flink-learn-project/src/main/scala/batch/WordCountBatch.scala
->>>>>>> 0ba8e892613cd3768f1b47f663371747d4c1b08f
 import org.apache.flink.api.scala.{AggregateDataSet, DataSet, ExecutionEnvironment}
 
 import org.apache.flink.api.scala._
 
-<<<<<<< HEAD
-=======
-object WordCountBatch {
-=======
-import org.apache.flink.api.scala._
->>>>>>> 09c976b8b8d54f165e2c80338b1e0268cf5436bb:flink-learn-project/src/main/scala/com/atguigu/wc/WordCount.scala
-
->>>>>>> 0ba8e892613cd3768f1b47f663371747d4c1b08f
 /*
  * @Author: "songzhanliang"
  * @Date: 2020/10/12 21:43
