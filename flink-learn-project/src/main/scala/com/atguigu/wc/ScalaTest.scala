e97e2287904413046c6c7e2721ac3c67928ea1a5
xjs513 <xjs513@163.com>
2020-11-03 19:51:43 +0800

    U

---
+++ flink-learn-project/src/main/scala/com/atguigu/wc/ScalaTest.scala
@@ -0,0 +1,12 @@
+package com.atguigu.wc
+
+/*
+ * @Author: "songzhanliang"
+ * @Date: 2020/11/2 19:55
+ * @Description: 
+ */
+object ScalaTest {
+  def main(args: Array[String]): Unit = {
+    println("Hello World.")
+  }
+}
