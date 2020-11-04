094d1d388735ddab306249aed9ed49e13e5930f8
xjs513 <xjs513@163.com>
2020-11-04 23:46:36 +0800

    update

---
+++ flink-learn-project/src/main/java/com/atguigu/apitest/SideOutputTest.java
@@ -0,0 +1,83 @@
+package com.atguigu.apitest;/*
+ * @Author: "songzhanliang"
+ * @Date: 2020/11/4 21:23
+ * @Description:
+ */
+
+import com.atguigu.modules.SensorReading;
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+import java.io.IOException;
+
+public class SideOutputTest {
+
+    static OutputTag<SensorReading> lateOutputTag = new OutputTag<SensorReading>("sideOutput"){};
+
+    public static void main(String[] args) {
+        // 1. 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 从调用时刻开始给 env 创建的每一个 stream 追加时间特征
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        env.setParallelism(1);
+
+        // 设置状态后端
+        try {
+            env.setStateBackend(new RocksDBStateBackend("", true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 2. 从文件读取数据 nc -l -p 13579
+        // String inputPath = "data/flink/sensor.txt";
+        // DataStreamSource<String> inputStream = env.readTextFile(inputPath);
+
+        // 从套接字读取数据
+        DataStream<String> inputStream = env.socketTextStream("localhost", 13579);
+
+        // 3. 做类型转换
+        DataStream<SensorReading> dataStream = inputStream
+                .map(line -> {
+                    String[] split = line.split(",");
+                    return new SensorReading(split[0], Long.parseLong(split[1]), Double.parseDouble(split[2]));
+                });
+
+        SingleOutputStreamOperator<SensorReading> result = dataStream.process(new SplitTempProcessor(30.0D));
+
+        result.print("result");
+
+        result.getSideOutput(lateOutputTag).print("sideOutput");
+
+        try {
+            env.execute("SideOutputTest");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class SplitTempProcessor extends ProcessFunction<SensorReading, SensorReading>{
+
+    private double threshold;
+
+    public SplitTempProcessor(double threshold){
+        this.threshold = threshold;
+    }
+
+    @Override
+    public void processElement(SensorReading sensorReading, Context context, Collector<SensorReading> collector) throws Exception {
+        if (sensorReading.getTemperature() > threshold){
+            collector.collect(sensorReading);
+        } else {
+            context.output(SideOutputTest.lateOutputTag, sensorReading);
+        }
+    }
+}
--- flink-learn-project/src/main/scala/com/atguigu/apitest/StateTestScala.scala
+++ flink-learn-project/src/main/scala/com/atguigu/apitest/StateTestScala.scala
@@ -1,51 +1,51 @@
-package com.atguigu.apitest
-
-import org.apache.flink.api.common.functions.FlatMapFunction
-import org.apache.flink.streaming.api.scala._
-import org.apache.flink.util.Collector
-
-
-
-/*
- * @Author: "songzhanliang"
- * @Date: 2020/11/3 21:26
- * @Description: 
- */
-object StateTestScala {
-  def main(args: Array[String]): Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-
-    env.setParallelism(1)
-
-    val lines: DataStream[String] = env.socketTextStream("localhost", 7777)
-
-//    lines.flatMap(new FlatMapFunction[String, SensorReading] {
-//      override def flatMap(t: String, collector: Collector[SensorReading]) = {
-//        val strings = t.split(", ")
-//        collector.collect(SensorReading(strings(0), strings(1).toLong, strings(2).toDouble))
-//      }
+//package com.atguigu.apitest
+//
+//import org.apache.flink.api.common.functions.FlatMapFunction
+//import org.apache.flink.streaming.api.scala._
+//import org.apache.flink.util.Collector
+//
+//
+//
+///*
+// * @Author: "songzhanliang"
+// * @Date: 2020/11/3 21:26
+// * @Description:
+// */
+//object StateTestScala {
+//  def main(args: Array[String]): Unit = {
+//    val env = StreamExecutionEnvironment.getExecutionEnvironment
+//
+//    env.setParallelism(1)
+//
+//    val lines: DataStream[String] = env.socketTextStream("localhost", 7777)
+//
+////    lines.flatMap(new FlatMapFunction[String, SensorReading] {
+////      override def flatMap(t: String, collector: Collector[SensorReading]) = {
+////        val strings = t.split(", ")
+////        collector.collect(SensorReading(strings(0), strings(1).toLong, strings(2).toDouble))
+////      }
+////    })
+//
+//    val dataStream = lines.map(line => {
+//      val strings = line.split(",")
+//      SensorReading(strings(0), strings(1).toLong, strings(2).toDouble)
 //    })
-
-    val dataStream = lines.map(line => {
-      val strings = line.split(",")
-      SensorReading(strings(0), strings(1).toLong, strings(2).toDouble)
-    })
-    val result = dataStream.keyBy(_.id)
-      .flatMapWithState[(String, Double, Double), Double] {
-        case (data: SensorReading, None) => (List.empty, Some(data.temperature))
-        case (data: SensorReading, lastTemp: Some[Double]) => {
-          val diff = (data.temperature - lastTemp.get).abs
-          if (diff > 10.0D)
-            (List((data.id, data.timestamp, data.temperature)), Some(data.temperature))
-          else
-            (List.empty, Some(data.temperature))
-        }
-      }
-
-    result.print()
-    
-    env.execute("StateTest")
-  }
-}
-
-case class SensorReading(id:String, timestamp:Long, temperature:Double)
+//    val result = dataStream.keyBy(_.id)
+//      .flatMapWithState[(String, Double, Double), Double] {
+//        case (data: SensorReading, None) => (List.empty, Some(data.temperature))
+//        case (data: SensorReading, lastTemp: Some[Double]) => {
+//          val diff = (data.temperature - lastTemp.get).abs
+//          if (diff > 10.0D)
+//            (List((data.id, data.timestamp, data.temperature)), Some(data.temperature))
+//          else
+//            (List.empty, Some(data.temperature))
+//        }
+//      }
+//
+//    result.print()
+//
+//    env.execute("StateTest")
+//  }
+//}
+//
+//case class SensorReading(id:String, timestamp:Long, temperature:Double)
