a45d12c492670fd667ecdf1acb09b4f12084f732
Kasa <xjs513@163.com>
2020-12-07 18:59:55 +0800

    flink update

---
+++ hive-learn-project/src/main/java/com/evente/GroovyTest.java
@@ -0,0 +1,21 @@
+package com.evente;
+
+import com.google.common.collect.HashBiMap;
+import groovy.lang.GroovyShell;
+
+/*
+ * @author : Kasa
+ * @date : 2020/12/4 15:07
+ * @description :
+ *
+ *  ./presto --server mosh-data-1:4444 --catalog hive --schema default
+ */
+public class GroovyTest {
+    public static void main(String[] args) {
+        GroovyShell groovyShell = new GroovyShell();
+        groovyShell.evaluate("println 'My First Groovy shell.'");
+
+        HashBiMap hashBiMap = new HashBiMap();
+
+    }
+}
